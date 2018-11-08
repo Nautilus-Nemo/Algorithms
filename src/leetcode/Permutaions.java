0a5fd058a7226abf83493d5f2f8e9a6ebeb66930
Nautilus-Nemo <13612262974@163.com>
2018-11-08 19:10:18 +0800

    push [leetcode] Permutaions -> 2

--- src/leetcode/Permutaions.java
+++ src/leetcode/Permutaions.java
@@ -1,17 +1,21 @@
 package leetcode;
+import jdk.jshell.execution.Util;
+
 import java.util.*;
 
 public class Permutaions {
-    public List permute(int[] num) {
+    /******************** solution 1 ******************************/
+
+    public List permuteS1(int[] num) {
         int len = num.length;
         List<String> res = new ArrayList<>();
         int[] visited = new int[len];
         Stack<Integer> out = new Stack<>();
-        permuteDFS(num,0,visited,out,res);
+        permuteDFS1(num,0,visited,out,res);
         return res;
     }
 
-    private void permuteDFS(int[] num_, int level, int[] visited_, Stack out_, List res_) {
+    private void permuteDFS1(int[] num_, int level, int[] visited_, Stack out_, List res_) {
         if (level == num_.length) res_.add(out_.toString());
         else {
             for (int i = 0; i < num_.length; i++) {
@@ -19,7 +23,7 @@ public class Permutaions {
                     visited_[i] = 1;
                     out_.push(num_[i]);
                     System.out.println("before" + i + " " + out_.toString());
-                    permuteDFS(num_, level + 1, visited_, out_, res_);
+                    permuteDFS1(num_, level + 1, visited_, out_, res_);
                     visited_[i] = 0;
                     out_.pop();
                     System.out.println("after" + i + " " + out_.toString());
@@ -28,13 +32,47 @@ public class Permutaions {
         }
     }
 
+    /******************** solution 2 ******************************/
+    public List permuteS2(Vector<Integer> num){
+        Stack<String> res = new Stack<>();
+        permuteDFS2(num,0,res);
+        return res;
+    }
+
+    private void permuteDFS2(Vector<Integer> num_,int start,Stack res_) {
+        if (start >= num_.size()) res_.push(num_.toString());
+        for (int i = start; i < num_.size(); i++) {
+            //swap between start and i
+            int tem = num_.get(start);
+            num_.set(start,num_.get(i));
+            num_.set(i,tem);
+            System.out.println("before" + i + " " + num_.toString());
+            permuteDFS2(num_,(start + 1),res_);
+            tem = num_.get(start);
+            num_.set(start,num_.get(i));
+            num_.set(i,tem);
+            System.out.println("after" + i + " " + num_.toString());
+        }
+    }
+
     public static void main(String[] args) {
-        int[] num = {1,2,3};
+        int[] num1 = {1,2,3};
         Permutaions testP = new Permutaions();
-        List permutions = testP.permute(num);
-        Iterator it = permutions.iterator();
-//        while (it.hasNext()){
-//            System.out.println(it.next());
+
+//        List permutions1 = testP.permuteS1(num1);
+//        Iterator it1 = permutions1.iterator();
+//        while (it1.hasNext()){
+//            System.out.println(it1.next());
+//        }
+
+        Vector<Integer> num2 = new Vector<>();
+        num2.add(1);
+        num2.add(2);
+        num2.add(3);
+        List permutions2 = testP.permuteS2(num2);
+//        Iterator it2 = permutions2.iterator();
+//        while (it2.hasNext()){
+//            System.out.println(it2.next());
 //        }
     }
 }
